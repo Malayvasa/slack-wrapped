4cb5dbc39ea0ef56011a81d3948847c4d40a02fb
Malay Vasa <malayvasa@gmail.com>
2025-11-24 14:16:48 +0530

    Update Slack Wrapped metrics for 2025-11-17 to 2025-11-24

--- data/mockData.ts
+++ data/mockData.ts
@@ -1,24 +1,24 @@
 import { SlackMetrics } from '@/types';
 
 export const mockMetrics: SlackMetrics = {
-  totalMessages: 207,
-  totalEmojiReactions: 61,
+  totalMessages: 217,
+  totalEmojiReactions: 41,
   mostActiveMember: {
     name: 'Lingala',
-    messageCount: 46,
+    messageCount: 42,
     avatar: 'https://avatars.slack-edge.com/2025-03-17/8614645842754_b5e7b4405ded6d39cdb5_192.jpg',
   },
   mostActiveChannel: {
     name: '#internal-glean',
-    messageCount: 58,
+    messageCount: 34,
   },
   mostPopularPost: {
-    text: 'Hi everyone! Please welcome Pranjali to the team! She will be working on Recruiting & HR stuff here at Composio! Previously she has worked at ShopOS, Rippling & CRED.',
-    author: 'Karan Vaidya',
-    channel: '#general',
-    timestamp: '2025-11-19T08:35:45Z',
-    reactions: 36,
-    replies: 0,
-    totalEngagement: 36,
+    text: 'Can we take master to prod?\n<@U06BF23F1T5|Himanshu> <@U08JJGWGULE|dhawal> <@U08NVJC15GF|Srujan>\n<https://github.com/ComposioHQ/hermes/pull/7111>',
+    author: 'Lingala',
+    channel: '#pod-platform',
+    timestamp: '2025-11-24T00:00:00Z',
+    reactions: 3,
+    replies: 6,
+    totalEngagement: 9,
   },
 };
